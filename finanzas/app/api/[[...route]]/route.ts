ac2250cf37ad51bb0a50ee58b4eed08e70dde911
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-05-16 15:46:16 -0300

    creado header

--- finanzas/app/api/[[...route]]/route.ts
+++ finanzas/app/api/[[...route]]/route.ts
@@ -1,24 +1,55 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { request } from 'http';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'//Proteger rutas verificando logeo
 
+
+import authors from "./authors"
+import books from "./books"
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-    .get('/hello', (c) => {
-        return c.json({
-            message: 'Hello Next.js!',
-        })
-        })
-    .get("/hello/:test", (c) => {
-        const test = c.req.param("test");
+app.route("/authors", authors);
+app.route("/books", books);
 
-        return c.json({
-            message: "Hello world",
-            test: test,
-        })
-    })
+
 export const GET = handle(app)
 export const POST = handle(app)
+
+
+
+
+
+// app
+//     .get('/hello', 
+//     clerkMiddleware(),                            //
+//     (c) => {
+//         const auth = getAuth(c);
+//         if(!auth?.userId){
+//             return c.json({error: "Sin acceso"}); // Verificar Logeo
+//         }
+
+//         return c.json({
+//             message: 'Hello Next.js!',
+//             userId: auth.userId
+//         })
+//         })                                         //
+
+//     .get("/hello/:test", (c) => {
+//         const test = c.req.param("test");
+
+//         return c.json({
+//             message: "Hello world",
+//             test: test,
+//         })
+//         })
+//     .post("/",
+//         zValidator("json", z.object({
+//             name: z.string(),
+//             userId: z.string(),
+//         })),
+//         (c) => {
+//             const {name, userId} = c.req.valid("json");
+//             return c.json({});
+//         }
+//     )
